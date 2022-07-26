654f1893e96809cb850a8eaff602fc21db694e6b
John Lonergan <john.lonergan@gmail.com>
2022-07-26 19:01:37 +0100

    added integerFlex which explores impact of fractional precision

--- integerFlex.c
+++ integerFlex.c
@@ -1,7 +1,13 @@
+
 /**
-  ascii Mandelbrot using fixed point integer maths with an "8.8" encoding.
+  ascii Mandelbrot using fixed point integer maths with a selectable fractional precision in bits.
+  This is still only 16 bits mathc and allocating more than 6 bits of fractional precision leads to an overflow.
 
   shorts (16 bit) chosen for compatibility with a later reimplementation as two bytes per number in assembler.
+  Frequent casts to short to ensure we're not accidentally benefitting from GCC promotion to long.
+
+  gcc integerFlex.c  -lm ; ./a.out | more
+
  */
 
 #include <stdio.h>
@@ -10,12 +16,7 @@
 #include <stdint.h>
 #include <string.h>
 
-void prt(short c, short s) {
-  printf("s=%d/%04x     ", s, (0xffff & s));
-  printf("c=%d dec/%02d trunc dec / %02x\n", c, 0xff & c, 0xff & c);
-}
-
-short num(double f, int precision) {
+short toPrec(double f, int bitsPrecision) {
 #define BYTE_TO_BINARY(byte)  \
   (byte & 0x200 ? '1' : '0'), \
   (byte & 0x100 ? '1' : '0'), \
@@ -28,8 +29,8 @@ short num(double f, int precision) {
   (byte & 0x02 ? '1' : '0'), \
   (byte & 0x01 ? '1' : '0')
 
-  short whole = ((short)floor(f) << (precision));
-  short part = (f-floor(f))*(pow(2,precision));
+  short whole = ((short)floor(f) << (bitsPrecision));
+  short part = (f-floor(f))*(pow(2,bitsPrecision));
   short ret = whole + part;
   printf(" %3f -> %3d 0x%04x  %c%c %c%c%c%c%c%c%c%c\n",f, ret, ret, BYTE_TO_BINARY(ret));
   return ret;
@@ -43,15 +44,15 @@ int main(int argc, char* argv[])
   short zoom=1;  // bigger with finer detail ie a smaller step size - leave at 1 for 32x22
 
   // params 
-  short precision = 6;
-  printf("DECS=%d\n", precision);
+  short bitsPrecision = 6;
+  printf("DECS=%d\n", bitsPrecision);
 
-  short X1 = num(3.5,precision) / zoom;
-  short X2 = num(2.25,precision) ;
+  short X1 = toPrec(3.5,bitsPrecision) / zoom;
+  short X2 = toPrec(2.25,bitsPrecision) ;
 
-  short Y1 = num(3,precision)/zoom ;   // horiz pos
-  short Y2 = num(1.5,precision) ; // vert pos
-  short LIMIT = num(4,precision);
+  short Y1 = toPrec(3,bitsPrecision)/zoom ;   // horiz pos
+  short Y2 = toPrec(1.5,bitsPrecision) ; // vert pos
+  short LIMIT = toPrec(4,bitsPrecision);
 
 
   // fractal
@@ -63,10 +64,8 @@ int main(int argc, char* argv[])
     short px=0;
     while (px < width*zoom) {
 
-      //short x0 = (((short)(px*X1)/zoom) / width) - X2;
-      //short y0 = (((short)(py*Y1)/zoom) / height) - Y2;
-      short x0 = (((short)(px*X1)) / width) - X2;
-      short y0 = (((short)(py*Y1)) / height) - Y2;
+      short x0 = (short)(((short)(px*X1)) / width) - X2;
+      short y0 = (short)(((short)(py*Y1)) / height) - Y2;
 
       short x=0;
       short y=0;
@@ -77,8 +76,8 @@ int main(int argc, char* argv[])
       short xSqr;
       short ySqr;
       while (i < iters) {
-        xSqr = (short)(x * x) >> precision;
-        ySqr = (short)(y * y) >> precision;
+        xSqr = ((short)(x * x)) >> bitsPrecision;
+        ySqr = ((short)(y * y)) >> bitsPrecision;
 
         // breakout also if negative as this indicates overflow of the addition which is a fault
         if ((xSqr + ySqr) > LIMIT || (xSqr+ySqr) < 0) {
@@ -86,7 +85,7 @@ int main(int argc, char* argv[])
         }
 
         short xt = xSqr - ySqr + x0;
-        y = (((x * y)>>precision) * 2 ) + y0;
+        y = ((((short)(x * y)) >> bitsPrecision) * 2 ) + y0;
         x=xt;
 
         i = i + 1;
