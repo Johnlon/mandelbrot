5effbf4a853b35277bd37b303b3804c6e727d6b3
John Lonergan <john.lonergan@gmail.com>
2022-07-26 18:54:45 +0100

    added integerFlex which explores impact of fractional precision

--- integerFlex.c
+++ integerFlex.c
@@ -15,7 +15,7 @@ void prt(short c, short s) {
   printf("c=%d dec/%02d trunc dec / %02x\n", c, 0xff & c, 0xff & c);
 }
 
-short num(double f, int dec) {
+short num(double f, int precision) {
 #define BYTE_TO_BINARY(byte)  \
   (byte & 0x200 ? '1' : '0'), \
   (byte & 0x100 ? '1' : '0'), \
@@ -28,8 +28,8 @@ short num(double f, int dec) {
   (byte & 0x02 ? '1' : '0'), \
   (byte & 0x01 ? '1' : '0')
 
-  short whole = ((short)floor(f) << (dec));
-  short part = (f-floor(f))*(pow(2,dec));
+  short whole = ((short)floor(f) << (precision));
+  short part = (f-floor(f))*(pow(2,precision));
   short ret = whole + part;
   printf(" %3f -> %3d 0x%04x  %c%c %c%c%c%c%c%c%c%c\n",f, ret, ret, BYTE_TO_BINARY(ret));
   return ret;
@@ -42,15 +42,16 @@ int main(int argc, char* argv[])
   short height = 22; // basic width of a zx81
   short zoom=1;  // bigger with finer detail ie a smaller step size - leave at 1 for 32x22
 
-  // params
-  short decs = 6;
-  printf("DECS=%d\n", decs);
-  short X1 = num(3.5,decs) / zoom;
-  short X2 = num(2.25,decs) ;
+  // params 
+  short precision = 6;
+  printf("DECS=%d\n", precision);
 
-  short Y1 = num(3,decs)/zoom ;   // horiz pos
-  short Y2 = num(1.5,decs) ; // vert pos
-  short LIMIT = num(4,decs);
+  short X1 = num(3.5,precision) / zoom;
+  short X2 = num(2.25,precision) ;
+
+  short Y1 = num(3,precision)/zoom ;   // horiz pos
+  short Y2 = num(1.5,precision) ; // vert pos
+  short LIMIT = num(4,precision);
 
 
   // fractal
@@ -76,8 +77,8 @@ int main(int argc, char* argv[])
       short xSqr;
       short ySqr;
       while (i < iters) {
-        xSqr = (short)(x * x) >> decs;
-        ySqr = (short)(y * y) >> decs;
+        xSqr = (short)(x * x) >> precision;
+        ySqr = (short)(y * y) >> precision;
 
         // breakout also if negative as this indicates overflow of the addition which is a fault
         if ((xSqr + ySqr) > LIMIT || (xSqr+ySqr) < 0) {
@@ -85,7 +86,7 @@ int main(int argc, char* argv[])
         }
 
         short xt = xSqr - ySqr + x0;
-        y = (((x * y)>>decs) * 2 ) + y0;
+        y = (((x * y)>>precision) * 2 ) + y0;
         x=xt;
 
         i = i + 1;
