b8d382d2543e9c469fda771f069a378ccc074898
John Lonergan <john.lonergan@gmail.com>
2022-08-29 01:28:50 +0100

    no zoom

--- integerFlex.c
+++ integerFlex.c
@@ -17,39 +17,38 @@
 #include <string.h>
 
 short s(short i);
-short toPrec(double f, int bitsPrecision);
+short toPrec(float f, int bitsPrecision);
+
+// CHOOSE THE NUMBER OF BITS OF PRECISION - 6 is the most I found useful
+short bitsPrecision = 6;
 
 int main(int argc, char* argv[])
 {
 int log=0;
 
-  // chosen to match https://www.youtube.com/watch?v=DC5wi6iv9io
+  // chosen to match https://www.youtube.com/watch?v=DC5wi5iv9io
   int width = 32; // basic width of a zx81
   int height = 22; // basic width of a zx81
-  int zoom=1;  // bigger with finer detail ie a smaller step size - leave at 1 for 32x22
 
-  // params 
-  short bitsPrecision = 6;
   printf("PRECISION=%d\n", bitsPrecision);
 
-  short X1 = toPrec(3.5,bitsPrecision) / zoom;
-  short X2 = toPrec(2.25,bitsPrecision) ;
-  short Y1 = toPrec(3,bitsPrecision)/zoom ;   // horiz pos
-  short Y2 = toPrec(1.5,bitsPrecision) ; // vert pos
+  short X1 = toPrec(3.5,bitsPrecision);
+  short X2 = toPrec(2.5,bitsPrecision) ;
+  short Y1 = toPrec(2,bitsPrecision);
+  short Y2 = toPrec(1,bitsPrecision) ; // vert pos
   short LIMIT = toPrec(4,bitsPrecision);
-
 
   // fractal
   //char * chr = ".:-=X$#@ ";
   //char * chr = "12345678 ";
-  char * chr = "123456789ABCD ";
+  char * chr = "123456789ABCDE ";
   //char * chr = ".,'~=+:;[/<&?oxOX#.";
   short maxIters = strlen(chr);
 
   short py=0;
-  while (py < height*zoom) {
+  while (py < height) {
     short px=0;
-    while (px < width*zoom) {
+    while (px < width) {
 
       short x0 = s(s(px*X1) / width) - X2;
       short y0 = s(s(py*Y1) / height) - Y2;
@@ -69,11 +68,12 @@ int log=0;
         // The overflow check is only needed for precisions of over 6 bits because for 7 and above the sums come out overflowed and negative therefore we always run to maxIters and we see nothing.
         // By including the overflow break out we can see the fractal again though with noise.
         short sum =(xSqr + ySqr);
-        if (sum >= LIMIT || sum < 0) {
+        if (sum > LIMIT) { // || sum < 0) {
           break;
         }
 
         short xt = xSqr - ySqr + x0;
+/*
 if (log == 26) {
   printf("\n");
   printf("i    %4x\n", i);
@@ -84,6 +84,7 @@ if (log == 26) {
   printf("m6 %4x\n", (s(x*y)>>6) & 0xffff);
   printf("m1 %4x\n", (s(s(x*y)>>6) <<1) & 0xffff);
 }
+*/
         y = s(s(s(x * y) >> bitsPrecision) << 1) + y0;
         x=xt;
 
@@ -92,10 +93,14 @@ if (log == 26) {
       i = i - 1;
 
 
+/*
 if (log == 26) {
   exit(1);
 }
-      printf("%c", chr[i]);
+*/
+      //printf("%c", chr[i]);
+      //print("\u001b[48;05;${cl}m  \u001b[0m")
+      printf("\033[48;05;%dm%02d\033[0m", i, i);
 
 log++;
       px = px + 1;
@@ -107,7 +112,7 @@ log++;
 }
 
 // convert decimal value to a fixed point value in the given precision
-short toPrec(double f, int bitsPrecision) {
+short toPrec(float f, int bitsPrecision) {
   short whole = ((short)floor(f) << (bitsPrecision));
   short part = (f-floor(f))*(pow(2,bitsPrecision));
   short ret = whole + part;
